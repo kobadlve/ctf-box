b05413cc2e41826d17fdc68e2b33dda47079c408
kobadlve <kobad.dogsy@gmail.com>
2019-05-22 16:45:16 +0900

    [Fix] check z3 existence

--- provision-script/cryptotools.sh
+++ provision-script/cryptotools.sh
@@ -8,12 +8,14 @@ dir=~/tools/crypto
 sudo -E apt install -y sagemath
 
 # z3
-git clone https://github.com/Z3Prover/z3.git ~/tools/crypto/z3
-cd ~/tools/crypto/z3
-python scripts/mk_make.py --python
-cd build
-make
-sudo make install
+if [ ! -e $dir/z3 ]; then
+  git clone https://github.com/Z3Prover/z3.git ~/tools/crypto/z3
+  cd ~/tools/crypto/z3
+  python scripts/mk_make.py --python
+  cd build
+  make
+  sudo make install
+fi
 
 # RsaCtfTool
 git clone https://github.com/Ganapati/RsaCtfTool.git ~/tools/crypto/RsaCtfTool
